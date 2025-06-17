ae1d3220a7e56b252b7a5c3729698447f8f0423a
noizyapps.dev <noizyapps.dev@outlook.com>
2025-06-17 02:07:03 +0200

    Corretto bug immagine utente

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # File upload configuration
-    UPLOAD_FOLDER = os.path.join('static', 'profile_pics')
+    UPLOAD_FOLDER = os.path.join('app/static', 'profile_pics')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
     
